ffa0cf19324413c49eae9d7b4923dfc2c17ab21b
Victor Quinn <victor@socialradar.com>
2016-06-17 14:12:18 -0400

    Now nominally functional

--- src/map.jsx
+++ src/map.jsx
@@ -3,6 +3,7 @@ import Leaflet from './leaflet/leaflet.js';
 import EsriLeaflet from 'esri-leaflet';
 
 import './leaflet/leaflet.css';
+import './map.css';
 
 console.log('MAP LOADED YO');
 
@@ -12,17 +13,14 @@ export default class Map extends Component {
     };
 
     componentDidMount() {
-        console.log(Leaflet);
-        Leaflet.map('map').setView([38.9043478, -77.0429411], 13);
+        var map = L.map('map').setView([38.9043478, -77.0429411], 13);
+        EsriLeaflet.basemapLayer('Streets').addTo(map);
     }
 
     render() {
         // @todo don't use id for map, this will break with multiples
         return (
-            <div>
-                <h3>MAP</h3>
-                <div id="map"></div>
-            </div>
+            <div id="map" className="react-esri-map"></div>
         );
     }
 }
