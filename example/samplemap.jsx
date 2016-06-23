3fb9a87315d40b3f4e2f111aa8f29f1a35a060c3
Victor Quinn <mail@victorquinn.com>
2016-06-23 15:24:02 -0400

    Add ESRI feature layer

--- example/samplemap.jsx
+++ example/samplemap.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactEsriMap, { BaseMapLayer, TiledMapLayer, GeoJSON } from '../src/index';
+import ReactEsriMap, { BaseMapLayer, FeatureLayer, GeoJSON, TiledMapLayer } from '../src/index';
 
 import './samplemap.css'
 
@@ -14,14 +14,31 @@ export default class SampleMap extends Component {
             }
         };
 
+        var options = {
+            url: 'https://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/stops/FeatureServer/0/'
+        };
+
         return (
             <div className='samplemap'>
                 <ReactEsriMap lat={38.9043478} lng={-77.0429411} >
                     <BaseMapLayer></BaseMapLayer>
                     <GeoJSON data={data}></GeoJSON>
+                    <FeatureLayer options={options}></FeatureLayer>
                 </ReactEsriMap>
             </div>
         );
     }
 }
 // <TiledMapLayer url="https://services.arcgisonline.com/ArcGIS/rest/services/USA_Topo_Maps/MapServer"></TiledMapLayer>
+
+/*
+   return (
+   <div className='samplemap'>
+   <ReactEsriMap lat={45.53} lng={-122.64} >
+   <BaseMapLayer></BaseMapLayer>
+   <GeoJSON data={data}></GeoJSON>
+   <FeatureLayer options={options}></FeatureLayer>
+   </ReactEsriMap>
+   </div>
+   );
+*/
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import BaseMapLayer from './basemaplayer';
+import FeatureLayer from './featurelayer';
 import GeoJSON from './geojson';
 import Map from './map';
 import TiledMapLayer from './tiledmaplayer';
 
 export {
     BaseMapLayer,
+    FeatureLayer,
     GeoJSON,
     Map,
     TiledMapLayer
